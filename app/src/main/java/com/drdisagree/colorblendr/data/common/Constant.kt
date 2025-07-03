93b5854ad340a7f14cadbd837a4330a26360baf0
hanubeki <hanubeki@gmail.com>
2025-07-03 10:41:44 +0900

    Attempt to get the bitmap from Muzei API

--- app/src/main/java/com/drdisagree/colorblendr/data/common/Constant.kt
+++ app/src/main/java/com/drdisagree/colorblendr/data/common/Constant.kt
@@ -33,6 +33,7 @@ object Constant {
     const val DOLBY_ATMOS = "org.lineageos.dspvolume.xiaomi"
     const val THEME_PICKER = "com.android.wallpaper"
     const val THEME_PICKER_GOOGLE = "com.google.android.apps.wallpaper"
+    const val MUZEI = "net.nurik.roman.muzei"
 
     // Request codes
     const val SHIZUKU_PERMISSION_REQUEST_ID = 100
